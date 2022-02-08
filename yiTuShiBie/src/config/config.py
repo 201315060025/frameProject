d04abbcea86d330327c5d00a5ec6cad24a6a72b6
bilixin <bilixin@4paradigm.com>
2022-02-08 18:29:25 +0800

    增加数据源

--- yiTuShiBie/src/config/config.py
+++ yiTuShiBie/src/config/config.py
@@ -9,3 +9,37 @@ class Config():
     # Application config
     TIMEZONE = 'Asia/Shanghai'
     BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+    # 文件路径
+    txtPath = r"/Users/4paradigm/code/person/fram_project/yiTuShiBie/src/data"
+    exePath = r"/Users/4paradigm/code/person/fram_project/yiTuShiBie/src/data"
+
+    # 测试数据
+    url = "t"
+    data = {
+    "requestId": "72f15ad1-19f8-430d-9826-e202ff01212b",
+    "accessToken": "mib",
+    "requestTime": 0,
+    "resultLimit": 1,
+    "commonFeatures": {},
+    "rawInstances": [
+        {
+            "id": "1",
+            "rawFeatures": {
+                "sepal_length": 5,
+                "sepal_width": 3,
+                "petal_length": 1.6,
+                "petal_width": 0.2
+            }
+        }
+    ],
+    "isDebug": False,
+    "isWarmupRequest": False
+    }
+
+    TEST = True
+    ip = ""
+    user = ''
+    pwd = ""
+    db_name = 'student'
+    table_name = ''
--- yiTuShiBie/src/views/rss.py
+++ yiTuShiBie/src/views/rss.py
@@ -1,33 +1,96 @@
 #!/usr/bin/env python
-from sanic import Sanic
+import json as js,os, requests, time
+
+from sanic import Sanic,request
 from sanic.response import json, text, html
 from feedparser import parse
 from jinja2 import Environment, PackageLoader, select_autoescape
-
 import sys
+from src.config.config import Config
 
-# https://github.com/channelcat/sanic/blob/5bb640ca1706a42a012109dc3d811925d7453217/examples/jinja_example/jinja_example.py
 # 开启异步特性  要求3.6+
 enable_async = sys.version_info >= (3, 6)
 # app = Sanic("ytsbCode")
 app = Sanic(name='_src-views')
 
-# jinjia2 config
-# env = Environment(
-#     loader=PackageLoader('views.rss', '../templates'),
-#     autoescape=select_autoescape(['html', 'xml', 'tpl']),
-#     enable_async=enable_async)
-
-
-# async def template(tpl, **kwargs):
-#     template = env.get_template(tpl)
-#     rendered_template = await template.render_async(**kwargs)
-#     return html(rendered_template)
-
 
 @app.route('/test')
 def test_api(request):
     return json({'status': 200, 'message': "success", 'data': None})
+
+
+def op_save_file(file_obj, file_type):
+    filedata = file_obj.get('file')
+    if filedata:
+        # 格式化数据名
+        name = "{0}_{1}.{2}".format('.'.join(filedata.name.split('.')[:-1]), str(int(time.time())), filedata.name.split('.')[-1])
+        try:
+            write_mode, file_path = ('wb',Config.exePath) if file_type == '2' else ('w', Config.txtPath)
+            file_name = os.path.join(file_path, name)
+            # 上传文件写入
+            with open(file_name, write_mode) as ff:
+                ff.write(filedata.body)
+        except IOError:
+            return 1, '上传文件 失败', ''
+        return 0, '上传文件成功, 文件名: {}'.format(file_name), file_name
+    else:
+        return 1,  '上传文件失败', ''
+
+
+def send_data(data):
+    """"""
+    url = Config.url
+    data = Config.data
+    res = requests.post(url, data=js.dumps(data), headers={})
+    if res.status_code == 200:
+        return js.loads(res.text)
+    return {}
+
+
+def read_file_data(file_name, file_type):
+    """读取数据"""
+    if file_name:
+        if file_type == '2':
+            # 读取exe数据
+            pass
+        else:
+            # 读取txt数据
+            pass
+    return ''
+
+
+def read_databse_data(db_config):
+    """读取数据库数据"""
+    if Config.TEST:
+        ip = Config.ip
+        user = Config.user
+        pwd = Config.pwd
+        db_name = Config.db_name
+        table_name = Config.table_name
+    else:
+        ip = db_config.get('ip')
+        user = db_config.get('name')
+        pwd = db_config.get('pwd')
+        db_name = db_config.get('db_name')
+        table_name = db_config.get('table_name')
+
+    con = pymysql.connect(host=ip,  # 此处必须是是127.0.0.1
+                          port=3306,
+                          user=user,  # mysql的登录账号admin
+                          password=pwd,  # mysql的登录密码pwd
+                          db=db_name,  # mysql中要访问的数据表
+                          charset='utf8')  # 表的字符集
+    sql = 'select * from {}'.format(table_name)
+    rows = c.execute(sql)  # 返回执行成功的结果条数
+    print(f'一共有 {rows} 张表')
+    for d in c.fetchall():
+        for k,v in d.items():
+            print(v)
+    return []
+
+
+
+
 
 
 
@@ -59,5 +122,39 @@ async def rss_html(request):
 
 @app.route('/intention-recognition-result', methods=['POST'])
 async def intention_recognition_result(request):
-    data={'status':200, 'message': 'OK', 'data': {'yi': "正常"}}
+    pars = dict(request.form)
+    # <editor-fold des="数据校验">
+    if 'modelType' not in pars:
+    # if 'modelType' not in pars or pars['modelType'][0] != 3:
+        return {'status':401, 'message': "参数错误", 'data': None}
+    if 'modelWeight' not in pars or 'ruleWeight' not in pars or 'dataSourceType' not in pars:
+        return {'status': 401, 'message': "参数错误", 'data': None}
+    if pars.get('dataSourceType') and pars['dataSourceType'][0] == '3' and 'ip' not in pars and 'name' not in pars and 'pwd' not in pars:
+        # 校验数据库
+        return {'status': 401, 'message': "参数错误", 'data': None}
+
+
+
+    modelWeight, ruleWeight = pars['modelWeight'][0], pars['ruleWeight'][0]
+    modelType, modelWeight, ruleWeight = pars['modelType'][0], pars['modelWeight'][0], pars['ruleWeight'][0]
+    dataSourceType = pars['dataSourceType'][0]
+
+    # print(request.files)
+    if dataSourceType == '2':
+        # 保存excel
+        save_flag, save_result, file_name = op_save_file(request.files,dataSourceType)
+        # 读取excel
+        new_data = read_file_data(file_name, '2')
+    elif dataSourceType == '1':
+        # 保存txt
+        save_flag, save_result, file_name = op_save_file(request.files,dataSourceType)
+        # 读取txt
+        new_data = read_file_data(file_name, '1')
+    else:
+        # 读取数据库
+        new_data = read_databse_data({})
+        pass
+    # 发送给模型
+    message = send_data({})
+    data={'status':200, 'message': 'OK', 'data': message}
     return json(data)
