53cfa7f66177a94bf02590b75a35b5e581412b79
bilixin <bilixin@4paradigm.com>
2022-02-11 11:33:31 +0800

    优化结构

--- yiTuShiBie/src/start.sh
+++ yiTuShiBie/src/start.sh
@@ -1,4 +1,4 @@
-le true
+whiile true
 do
     count=`ps -ef|grep run.py | grep -v grep`
     if [ "$?" != "0" ]
@@ -6,7 +6,7 @@ do
         echo "$(date "+%Y-%m-%d %H:%M:%S") > 程序挂掉， 开始restart" >> log.tt
         cd /root/blx/tornado_frame_project/yiTuShiBie/src && /root/miniconda3/bin/python run.py >> run.log 2 >&1
     else
-        echo " $(date "+%Y-%m-%d %H:%M:%S") > tomcat is running..."
+        echo " $(date "+%Y-%m-%d %H:%M:%S") > tomcat is running..." >> log.tt
     fi
     sleep 10
 done
--- yiTuShiBie/src/views/rss.py
+++ yiTuShiBie/src/views/rss.py
@@ -7,6 +7,7 @@ from feedparser import parse
 from jinja2 import Environment, PackageLoader, select_autoescape
 import sys
 from src.config.config import Config
+from .baseView import *
 
 # 开启异步特性  要求3.6+
 enable_async = sys.version_info >= (3, 6)
@@ -17,82 +18,6 @@ app = Sanic(name='_src-views')
 @app.route('/test')
 def test_api(request):
     return json({'status': 200, 'message': "success", 'data': None})
-
-
-def op_save_file(file_obj, file_type):
-    write_mode, file_path, file_key = ('wb', Config.exePath, 'excelAddress') if file_type == '2' else ('w', Config.txtPath, 'txtAddress')
-    filedata = file_obj.get(file_key)
-    if filedata:
-        # 格式化数据名
-        name = "{0}_{1}.{2}".format('.'.join(filedata.name.split('.')[:-1]), str(int(time.time())), filedata.name.split('.')[-1])
-        try:
-
-            file_name = os.path.join(file_path, name)
-            # 上传文件写入
-            with open(file_name, write_mode) as ff:
-                ff.write(filedata.body)
-        except IOError:
-            return 1, '上传文件 失败', ''
-        return 0, '上传文件成功, 文件名: {}'.format(file_name), file_name
-    else:
-        return 1,  '上传文件失败', ''
-
-
-def send_data(data):
-    """"""
-    url = Config.url
-    data = Config.data
-    res = requests.post(url, data=js.dumps(data), headers={})
-    if res.status_code == 200:
-        return js.loads(res.text)
-    return {}
-
-
-def read_file_data(file_name, file_type):
-    """读取数据"""
-    if file_name:
-        if file_type == '2':
-            # 读取exe数据
-            pass
-        else:
-            # 读取txt数据
-            pass
-    return ''
-
-
-def read_databse_data(db_config):
-    """读取数据库数据"""
-    if Config.TEST:
-        ip = Config.ip
-        user = Config.user
-        pwd = Config.pwd
-        db_name = Config.db_name
-        table_name = Config.table_name
-    else:
-        ip = db_config.get('ip')
-        user = db_config.get('name')
-        pwd = db_config.get('pwd')
-        db_name = db_config.get('db_name')
-        table_name = db_config.get('table_name')
-
-    con = pymysql.connect(host=ip,  # 此处必须是是127.0.0.1
-                          port=3306,
-                          user=user,  # mysql的登录账号admin
-                          password=pwd,  # mysql的登录密码pwd
-                          db=db_name,  # mysql中要访问的数据表
-                          charset='utf8')  # 表的字符集
-    sql = 'select * from {}'.format(table_name)
-    rows = c.execute(sql)  # 返回执行成功的结果条数
-    print(f'一共有 {rows} 张表')
-    for d in c.fetchall():
-        for k,v in d.items():
-            print(v)
-    return []
-
-
-
-
-
 
 
 @app.route("/")
